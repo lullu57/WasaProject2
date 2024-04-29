08518ccfaead2c16612172ebeda56a8290c46acc
lullu57 <kezitogaleacurmi@gmail.com>
2024-04-29 19:14:54 +0200

    added import

--- webui/src/router/index.js
+++ webui/src/router/index.js
@@ -1,5 +1,6 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ProfileView from '../views/ProfileView.vue'
 
 const router = createRouter({
 	history: createWebHashHistory(import.meta.env.BASE_URL),

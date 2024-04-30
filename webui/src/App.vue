514498e0748a68abcc829da88ea7fe9c9559551a
lullu57 <kezitogaleacurmi@gmail.com>
2024-04-30 18:17:08 +0200

    Put upload image below rest

--- webui/src/App.vue
+++ webui/src/App.vue
@@ -55,8 +55,8 @@ function logout() {
       </nav>
 
       <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-		<UploadImage v-if="isAuthenticated"/>
         <RouterView />
+        <UploadImage v-if="isAuthenticated"/>
       </main>
     </div>
   </div>

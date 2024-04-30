eaf4f4ccd407917aa30cad29de84eb07b7f14ce1
lullu57 <kezitogaleacurmi@gmail.com>
2024-04-30 10:57:02 +0200

    added Login Page, and userId in local storage

---
+++ webui/src/views/LoginView.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="login-container">
+      <h1>Login</h1>
+      <input v-model="username" placeholder="Enter username" />
+      <button @click="login">Login</button>
+      <p v-if="error">{{ error }}</p>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        username: '',
+        error: ''
+      };
+    },
+    methods: {
+      async login() {
+        try {
+          const response = await axios.post('/session', { name: this.username });
+          localStorage.setItem('userId', response.data.identifier);
+          this.$router.push('/'); // Redirect to home or dashboard
+        } catch (err) {
+          this.error = 'Failed to login. Please try again.';
+          console.error(err);
+        }
+      }
+    }
+  }
+  </script>
+  
+  <style>
+  /* Add styles here */
+  </style>
+  

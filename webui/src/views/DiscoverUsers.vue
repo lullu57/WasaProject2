e9cb669ed09eebc555a2fa715fd721e2ec07fb60
lullu57 <kezitogaleacurmi@gmail.com>
2024-05-01 15:38:47 +0200

    added checks and visual

--- webui/src/views/DiscoverUsers.vue
+++ webui/src/views/DiscoverUsers.vue
@@ -4,7 +4,9 @@
       <ul class="user-list">
         <li v-for="user in users" :key="user.userId">
           {{ user.username }}
-          <button @click="followUser(user.userId)">Follow</button>
+          <button @click="toggleFollow(user)" :disabled="user.processing">
+            {{ user.isFollowing ? 'Unfollow' : 'Follow' }}
+          </button>
         </li>
       </ul>
     </div>
@@ -28,19 +30,47 @@
           const response = await api.get('/users', {
             headers: { Authorization: localStorage.getItem('userId') }
           });
-          this.users = response.data;
+          this.users = response.data.map(user => ({
+            ...user,
+            isFollowing: false, // default to false, will be updated
+            processing: false
+          }));
+          await this.checkFollowStatus();
         } catch (error) {
           console.error('Failed to fetch users:', error);
         }
       },
-      async followUser(userId) {
+      async checkFollowStatus() {
         try {
-          await api.post(`/users/follows/${userId}`, {}, {
-            headers: { Authorization: localStorage.getItem('userId') }
-          });
-          alert('Followed successfully!');
+          // Get following status for each user
+          for (const user of this.users) {
+            const res = await api.get(`/follows/${user.userId}`, {
+              headers: { Authorization: localStorage.getItem('userId') }
+            });
+            user.isFollowing = res.data.isFollowed;
+          }
         } catch (error) {
-          console.error('Failed to follow user:', error);
+          console.error('Failed to fetch follow status:', error);
+        }
+      },
+      async toggleFollow(user) {
+        user.processing = true; // Indicate processing
+        try {
+          if (user.isFollowing) {
+            await api.delete(`/users/follows/${user.userId}`, {
+              headers: { Authorization: localStorage.getItem('userId') }
+            });
+            user.isFollowing = false;
+          } else {
+            await api.post(`/users/follows/${user.userId}`, {}, {
+              headers: { Authorization: localStorage.getItem('userId') }
+            });
+            user.isFollowing = true;
+          }
+        } catch (error) {
+          console.error('Failed to toggle follow:', error);
+        } finally {
+          user.processing = false; // Processing done
         }
       }
     }
@@ -59,6 +89,12 @@
   
   .user-list li {
     margin-bottom: 10px;
+    display: flex;
+    align-items: center;
+  }
+  
+  button {
+    margin-left: 10px;
   }
   </style>
   
